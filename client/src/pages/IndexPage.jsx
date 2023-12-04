e09110faff539cfa6828a022066dccc666963cd0
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-04 16:09:31 +0300

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Post from "../components/Post";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const IndexPage = () => {
   const [posts, setPosts] = useState([]);
