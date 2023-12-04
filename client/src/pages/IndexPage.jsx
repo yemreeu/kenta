11a4025607913abd0abcd2cbf20ccde981068555
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-04 16:07:42 +0300

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -3,18 +3,36 @@ import Post from "../components/Post";
 
 const IndexPage = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    fetch("https://kenta-be.vercel.app/post").then((response) => {
-      response.json().then((posts) => setPosts(posts));
-    });
+    setLoading(true);
+    fetch("https://kenta-be.vercel.app/post")
+      .then((response) => response.json())
+      .then((posts) => {
+        setPosts(posts);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
       <h1>Bloglar</h1>
       <br />
-      {posts.length > 0 && posts.map((post,index) => (
-        <Post key={index} {...post}/>
-      ))}
+      {loading ? (
+        <div>
+          <ClipLoader
+            color={"B06161"}
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+          <p>Loading ...</p>
+        </div>
+      ) : (
+        posts.length > 0 &&
+        posts.map((post, index) => <Post key={index} {...post} />)
+      )}
     </div>
   );
 };
