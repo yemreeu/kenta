1aa449e2fae0c5fc693aa55bbc550ece47de37d6
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-05 15:03:44 +0300

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,7 +7,7 @@ const RegisterPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://kenta-be.vercel.app/register", {
+    const response = await fetch("https://kenta-api.vercel.app/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
