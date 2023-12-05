6631de8fd66ac68c5c27b1f5efdfde1ae53c76cc
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-05 15:04:20 +0300

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -12,7 +12,7 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://kenta-be.vercel.app/login", {
+    const response = await fetch("https://kenta-api.vercel.app/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
