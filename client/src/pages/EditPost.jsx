62e3177ba9e155c069372a95b1be8e7ddedcbf9d
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-05 15:05:02 +0300

    Update EditPost.jsx

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -36,7 +36,7 @@ const EditPost = () => {
   const [redirect, setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch(`https://kenta-be.vercel.app/post/${id}`)
+    fetch(`https://kenta-api.vercel.app/post/${id}`)
       .then((response) => response.json())
       .then((postInfo) => {
         setTitle(postInfo.title);
@@ -56,7 +56,7 @@ const EditPost = () => {
     if (files?.[0]) {
       data.set("file", files?.[0]);
     }
-    const response = await fetch("https://kenta-be.vercel.app/post/" + id, {
+    const response = await fetch("https://kenta-api.vercel.app/post/" + id, {
       method: "PUT",
       body: data,
       credentials: "include",
